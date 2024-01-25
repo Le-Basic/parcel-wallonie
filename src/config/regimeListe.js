35fc596e3ed0c542af567ebe4ec6fe00dda3b064
mathieulebasic <mathieu@lebasic.com>
2024-01-25 09:47:21 +0100

    feat: ajouter page pour test benjamin

--- src/config/regimeListe.js
+++ src/config/regimeListe.js
@@ -5,28 +5,33 @@ export const regimeListe = [
     nomCourt: "actuel",
     libelle: "Régime actuel",
     default: true,
+    id: 0,
   },
   {
     nom: "Diminution d'1/4 des produits animaux",
     img: require("@/assets/img/parcours/regime-quart.svg"),
     nomCourt: "quart",
     libelle: "Diminution d'1/4 des produits animaux",
+    id: 1,
   },
   {
     nom: "Diminution de moitié des produits animaux",
     img: require("@/assets/img/parcours/regime-moitie.svg"),
     nomCourt: "moitie",
     libelle: "Diminution de moitié des produits animaux",
+    id: 2,
   },
   {
     nom: "VÉGÉTARIEN",
     img: require("@/assets/img/parcours/regime-vegetarien.svg"),
     nomCourt: "vegetarien",
     libelle: "Végétarien",
+    id: 3,
   },
   {
     nom: "VÉGÉTALIEN",
     img: require("@/assets/img/parcours/regime-vegetalien.svg"),
     nomCourt: "vegetalien",
+    id: 4,
   },
 ];
---
+++ src/views/testBenjamin.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>Salut Benjam</h2>
+  Store:
+  <pre>{{ $store.state }} </pre>
+  <pre v-if="surfaces"> {{ surfaces }}</pre>
+</template>
+
+<script>
+import { getSurfaceAMobiliser } from "@/plugins/getSurfacesNecessaires";
+
+export default {
+  name: "testBenjamin",
+  data() {
+    return {
+      surfaces: null,
+    };
+  },
+  async mounted() {
+    this.surfaces = await getSurfaceAMobiliser();
+  },
+};
+
+// <!-- enrichir la fonction getSurfaceAMobiliser qui est pour l'instant fausse
+// - le surfaces_a_mobiliser est une somme des surface_necessaire_conventionnel alors que ca doit être cette fonction:<template>
+//   partbioelevage * surface necessaire bio elevage + (1 - partbioelevage) * surface necessaire conventionnel + surface necessaire bio vegetal * partbiovegetal + surface necessaire bio vegetal * (1 - partbiovegetal) ... ca pour les 4 postes
+// - les part bios sont dans le store qu'on utilise qui s'appelle vuex
+// - il faudrait aussi ajouter dans la liste surfaces_a_mobiliser_parcel_niveau_1 pour chaque item la part  -->
+</script>
+
+<style></style>
