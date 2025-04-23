22797beb9acc7cfa8c69a825a7dd8dc4eff13d86
mathieulebasic <mathieu@lebasic.com>
2025-04-23 09:43:54 +0200

    feat: Changer texte emploi Legumes

--- src/views/modal/modalEmplois/emploiLegumes.vue
+++ src/views/modal/modalEmplois/emploiLegumes.vue
@@ -137,7 +137,7 @@
                     </li>
                     <li>
                       <strong
-                        >Parcel comptabilise uniquement les emplois
+                        >PARCEL-Wallonie comptabilise uniquement les emplois
                         agricoles</strong
                       >. La relocalisation de l’alimentation impacte aussi les
                       emplois du milieu et de l’aval des chaînes alimentaires.
@@ -147,10 +147,10 @@
                     <li>
                       <strong
                         >Les produits pris en compte par PARCEL-Wallonie
-                        représentent environ 91% de notre alimentation (en
+                        représentent environ 84% de notre alimentation (en
                         volume)</strong
                       >. Ce sont les produits recensés par la Statistique
-                      Agricole Annuelle d’Agreste. PARCEL-Wallonie ne prend pas
+                      Agricole Annuelle de Statbel. PARCEL-Wallonie ne prend pas
                       en compte :
                       <ul>
                         <li>
@@ -170,11 +170,11 @@
                     </li>
                     <li>
                       <strong
-                        >Parcel prend en compte les produits qui ont
+                        >PARCEL-Wallonie prend en compte les produits qui ont
                         actuellement une production représentative en Région
                         wallonne </strong
                       >(dont la production est recensée au sein des statistiques
-                      agricoles). La nomenclature retenue de 63 produits, ne
+                      agricoles). La nomenclature retenue de 40 produits, ne
                       représente pas l’ensemble de l’alimentation des wallon (il
                       manque notamment les boissons et les produits tropicaux).
                     </li>
@@ -206,15 +206,16 @@
                 <ul>
                   <li>
                     <strong
-                      >On utilise les données du dernier recensement agricole
-                      (2010), qui donne par Orientations technico-économique,
-                      les Unités de travail annuel par hectare</strong
+                      >On utilise les données statistiques de STATBEL (2020),
+                      qui donne par Orientations technico-économique, les Unités
+                      de travail annuel par hectare</strong
                     >. Les données seront mises à jour avec le prochain
                     recensement agricole.
                   </li>
                   <li>
                     <strong
-                      >Une exploitation est spécialisée au sein d’un OTE</strong
+                      >Une exploitation est spécialisée au sein d’un
+                      OTEX</strong
                     >
                     si la Production Brute Standard (indicateur économique sur
                     le potentiel de production d’une exploitation) de la ou des
@@ -223,7 +224,7 @@
                   <li>
                     <strong
                       >Pour le maraîchage, on utilise l’indicateur UTA/ha de
-                      l’OTE « Légumes et champignons ».</strong
+                      l’OTEX « Légumes et champignons ».</strong
                     >
                   </li>
                   <li>
@@ -233,48 +234,6 @@
                       href="/methodologie-de-calcul-de-la-relocalisation-alimentaire"
                       >page annexe méthodologique</a
                     >, accessible par le menu principal.
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-header" id="sources">
-              <button
-                class="btn btn-link btn-link-open"
-                data-toggle="collapse show"
-                href="#collapseSource"
-                aria-expanded="false"
-                data-target="#collapseSource"
-              >
-                <div><span class="icon-source icon white"></span></div>
-                <div>Principales sources de données</div>
-              </button>
-            </div>
-            <div
-              id="collapseSource"
-              class="collapse show"
-              data-parent="#accordion"
-              aria-labelledby="sources"
-            >
-              <div class="card-body">
-                <ul>
-                  <li>
-                    ­<span class="source-titre"
-                      ><a
-                        href="https://statbel.fgov.be/fr/themes/agriculture-peche/exploitations-agricoles-et-horticoles#figures"
-                        >Chiffres Agricoles</a
-                      > </span
-                    ><span class="source-auteur"> – Statbel</span
-                    ><span class="source-année"></span>
-                    <div>
-                      <a
-                        class=""
-                        href="https://statbel.fgov.be/fr/themes/agriculture-peche/exploitations-agricoles-et-horticoles#figures"
-                        >Voir la page de statistiques</a
-                      >
-                    </div>
                   </li>
                 </ul>
               </div>
